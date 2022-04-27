3ca67553a47d9516e1a339d2a175a924d8a635da
pingusl <pingusl@yahoo.fr>
2022-04-27 17:42:07 +0200

    V0.2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./components/header";
 import Main from "./components/main";
-
+import Footer from "./components/footer";
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
